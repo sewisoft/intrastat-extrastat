213c9f98d791275db7057e97a655fdc4930e5b53
Günter Selbert <guenter.selbert@sewisoft.de>
2021-11-25 22:19:34 +0100

    [FIX] api depends on it self

--- intrastat_product/models/intrastat_transport_mode.py
+++ intrastat_product/models/intrastat_transport_mode.py
@@ -1,3 +1,4 @@
+# Copyright 2021 sewisoft (https://sewisoft.de)
 # Copyright 2011-2017 Akretion (http://www.akretion.com)
 # Copyright 2009-2019 Noviat (http://www.noviat.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
@@ -9,22 +10,20 @@ from odoo import api, fields, models
 class IntrastatTransportMode(models.Model):
     _name = 'intrastat.transport_mode'
     _description = "Intrastat Transport Mode"
-    _rec_name = 'display_name'
     _order = 'code'
     _sql_constraints = [(
         'intrastat_transport_code_unique',
         'UNIQUE(code)',
         'Code must be unique.')]
 
-    display_name = fields.Char(
-        string='Display Name', compute='_compute_display_name', store=True,
-        readonly=True)
     code = fields.Char(string='Code', required=True)
     name = fields.Char(string='Name', required=True, translate=True)
     description = fields.Char(string='Description', translate=True)
 
-    @api.multi
     @api.depends('name', 'code')
-    def _compute_display_name(self):
-        for this in self:
-            this.display_name = '%s. %s' % (this.code, this.name)
+    def name_get(self):
+        res = []
+        for mode in self:
+            name = '%s. %s' % (mode.code, mode.name)
+            res.append((mode.id, name))
+        return res
